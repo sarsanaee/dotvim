17c96ed5c16038a7ec44d01a0a2e70d9ea0a0912
Alireza <sarsanaee@gmail.com>
2020-07-17 07:56:17 -0600

    setup script added * still not complete yet, but working

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo $DIR
+
+sudo apt -y install clang clang-format
+
+git submodule init
+git submodule update
+
+cd ..
+
+# I need to come with something here, I'm sure it is a wrong way.
+mv $DIR .vim
+ln -s ~/.vim/vimrc ~/.vimrc
+
+
