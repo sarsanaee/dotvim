12f8ed73aa549304563e7ef7ebe530f5a319e9ac
Alireza Sanaee <sarsanaee@gmail.com>
2024-10-02 10:56:15 +0100

    adding mac

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
-# required packages
-sudo apt update
-sudo apt -y install clang cmake ripgrep
+if [[ "$(uname)" == "Darwin" ]]; then
+    echo "Running on macOS"
+    brew install ripgrep clang cmake
+# Check if the OS is Ubuntu
+elif [[ "$(lsb_release -is)" == "Ubuntu" ]]; then
+    echo "Running on Ubuntu"
+    sudo apt update
+    sudo apt install clang cmake ripgrep
+else
+    echo "This script is intended to run on macOS and Ubuntu only."
+fi
 
 # install plug
 curl -fLo ~/.vim/autoload/plug.vim -k --create-dirs \
